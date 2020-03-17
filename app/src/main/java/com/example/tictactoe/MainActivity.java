3448ebcfead0db6d2120c9494d76a7be62fe50a2
Nadine Groot-Bernink <nadine.groot-bernink@mediabeam.com>
2020-03-17 21:28:05 +0100

    Zwischenstand

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.tictactoe;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +13,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setSymbolStart();
 
+
+    }
+
+    public void setSymbol(View view) {
+
+        ImageView symImageView = findViewById(R.id.s1img);
+
+        symImageView.animate().translationYBy(100).rotationY(3 * 360).setDuration(1000);
+
+
+    }
+
+    public void setSymbolStart() {
+        ImageView symbolImageView = findViewById(R.id.s1img);
+        symbolImageView.setY(-100);
+        symbolImageView = findViewById(R.id.s2img);
+        symbolImageView.setY(-1000);
+        symbolImageView = findViewById(R.id.s2img);
+        symbolImageView.setY(-1000);
+        symbolImageView = findViewById(R.id.s3img);
+        symbolImageView.setY(-1000);
+        symbolImageView = findViewById(R.id.s4img);
+        symbolImageView.setY(-1000);
+        symbolImageView = findViewById(R.id.s5img);
+        symbolImageView.setY(-1000);
+        symbolImageView = findViewById(R.id.s6img);
+        symbolImageView.setY(-1000);
+        symbolImageView = findViewById(R.id.s7img);
+        symbolImageView.setY(-1000);
+        symbolImageView = findViewById(R.id.s8img);
+        symbolImageView.setY(-1000);
+        symbolImageView = findViewById(R.id.s9img);
+        symbolImageView.setY(-1000);
     }
 }
