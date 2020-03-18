b83c875d104d22d81678e62e4c420cf1f424deb0
Nadine Groot-Bernink <nadine.groot-bernink@mediabeam.com>
2020-03-18 13:35:31 +0100

    Zwischenstand2

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -13,40 +13,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setSymbolStart();
+    }
+
+    public void dropIn(View view) {
+
+        ImageView counter = (ImageView) view;
+
+        counter.setTranslationY(-1500);
+
+        counter.setImageResource(R.drawable.sun);
+
+        counter.animate().translationYBy(1500).rotation(10 * 360).setDuration(1000);
+
+
+
+
 
 
     }
 
-    public void setSymbol(View view) {
 
-        ImageView symImageView = findViewById(R.id.s1img);
-
-        symImageView.animate().translationYBy(100).rotationY(3 * 360).setDuration(1000);
-
-
-    }
-
-    public void setSymbolStart() {
-        ImageView symbolImageView = findViewById(R.id.s1img);
-        symbolImageView.setY(-100);
-        symbolImageView = findViewById(R.id.s2img);
-        symbolImageView.setY(-1000);
-        symbolImageView = findViewById(R.id.s2img);
-        symbolImageView.setY(-1000);
-        symbolImageView = findViewById(R.id.s3img);
-        symbolImageView.setY(-1000);
-        symbolImageView = findViewById(R.id.s4img);
-        symbolImageView.setY(-1000);
-        symbolImageView = findViewById(R.id.s5img);
-        symbolImageView.setY(-1000);
-        symbolImageView = findViewById(R.id.s6img);
-        symbolImageView.setY(-1000);
-        symbolImageView = findViewById(R.id.s7img);
-        symbolImageView.setY(-1000);
-        symbolImageView = findViewById(R.id.s8img);
-        symbolImageView.setY(-1000);
-        symbolImageView = findViewById(R.id.s9img);
-        symbolImageView.setY(-1000);
-    }
 }
