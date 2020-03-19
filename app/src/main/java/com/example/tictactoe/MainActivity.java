4b569779b358f7f95ce5aa8f1d2ba08cefc399fe
Nadine Groot-Bernink <nadine.groot-bernink@mediabeam.com>
2020-03-19 10:18:12 +0100

    Zwischenstand3

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.tictactoe;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    int activePlayer = 1;
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+    int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +24,23 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView counter = (ImageView) view;
 
+        Log.i("Tag", counter.getTag().toString());
+
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
+
+        gameState[tappedCounter] = activePlayer;
+
         counter.setTranslationY(-1500);
 
-        counter.setImageResource(R.drawable.sun);
+        if (activePlayer == 0) {
+            counter.setImageResource(R.drawable.ximg);
+            activePlayer = 1;
+        } else if (activePlayer == 1) {
+            counter.setImageResource(R.drawable.sun);
+            activePlayer = 0;
+        }
 
-        counter.animate().translationYBy(1500).rotation(10 * 360).setDuration(1000);
+        counter.animate().translationYBy(1500).rotation(10 * 360).setDuration(500);
 
 
 
