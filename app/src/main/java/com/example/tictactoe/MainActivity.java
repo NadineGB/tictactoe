a5f72783b9d95177f53b4643ac24358992bb794e
Nadine Groot-Bernink <nadine.groot-bernink@mediabeam.com>
2020-03-20 22:33:21 +0100

    Finish

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -1,69 +1,111 @@
 package com.example.tictactoe;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    int activePlayer = 1;
+    int activePlayer = 0;
     int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
     int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
+    boolean gameActive = true;
+
+    public void dropIn(View view) {
+
+        ImageView counter = (ImageView) view;
+
+        // Log.i("Tag", counter.getTag().toString());
+
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
+
+        Button playAgainButton = (Button) findViewById(R.id.playAgainButton);
+        TextView winnerTextView = (TextView) findViewById(R.id.winnerTextView);
+
+        if (gameState[tappedCounter] == 2 && gameActive) {
+
+            gameState[tappedCounter] = activePlayer;
+            counter.setTranslationY(-1500);
+
+            if (activePlayer == 0) {
+                counter.setImageResource(R.drawable.ximg);
+                activePlayer = 1;
+            } else if (activePlayer == 1) {
+                counter.setImageResource(R.drawable.sun);
+                activePlayer = 0;
+            }
+            counter.animate().translationYBy(1500).rotation(10 * 360).setDuration(500);
+
+            for (int[] winningPosition : winningPositions) {
+
+                if (gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]] && gameState[winningPosition[0]] != 2) {
+
+                    String winner = "";
+
+                    gameActive = false;
+
+                    if (activePlayer == 1) {
+                        winner = "X";
+                    } else if (activePlayer == 0) {
+                        winner = "Sun";
+                    }
+
+                    //Toast.makeText(this, winner, Toast.LENGTH_LONG).show();
+
+                    winnerTextView.setText(winner + " hat gewonnen!");
+
+                    winnerTextView.setVisibility(View.VISIBLE);
+                    playAgainButton.setVisibility(View.VISIBLE);
+                } else {
+                    gameActive = false;
+
+                    for (int counterState : gameState) {
+                        if (counterState == 2) gameActive = true;
+                    }
+
+                    if (!gameActive) {
+                        winnerTextView.setText("Unendschieden!");
+
+                        winnerTextView.setVisibility(View.VISIBLE);
+                        playAgainButton.setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+        }
+    }
+
+    public void playAgain(View view) {
+
+        Button playAgainButton = (Button) findViewById(R.id.playAgainButton);
+        TextView winnerTextView = (TextView) findViewById(R.id.winnerTextView);
+
+        winnerTextView.setVisibility(View.INVISIBLE);
+        playAgainButton.setVisibility(View.INVISIBLE);
+
+        androidx.gridlayout.widget.GridLayout gridLayout = findViewById(R.id.gridLayout);
+
+        //Log.i("Tag", String.valueOf(gridLayout));
+
+        for (int i = 0; i < gridLayout.getChildCount(); i++) {
+
+            ImageView counter = (ImageView) gridLayout.getChildAt(i);
+            counter.setImageDrawable(null);
+
+        }
+        for (int i = 0; i < gameState.length; i++) {
+            gameState[i] = 2;
+        }
+        activePlayer = 0;
+        gameActive = true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    public void dropIn(View view) {
-
-        ImageView counter = (ImageView) view;
-
-        Log.i("Tag", counter.getTag().toString());
-
-        int tappedCounter = Integer.parseInt(counter.getTag().toString());
-
-        gameState[tappedCounter] = activePlayer;
-
-        counter.setTranslationY(-1500);
-
-        if (activePlayer == 0) {
-            counter.setImageResource(R.drawable.ximg);
-            activePlayer = 1;
-        } else if (activePlayer == 1) {
-            counter.setImageResource(R.drawable.sun);
-            activePlayer = 0;
-        }
-
-        counter.animate().translationYBy(1500).rotation(10 * 360).setDuration(500);
-
-        for (int[] winningPosition : winningPositions) {
-
-            if (gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]] && gameState[winningPosition[0]] != 2) {
-
-                String winner = "";
-
-                if (activePlayer == 1) {
-                    winner = "X";
-                } else if (activePlayer == 0) {
-                    winner = "Sun";
-                }
-
-                Toast.makeText(this, winner + " hat gewonnen", Toast.LENGTH_LONG).show();
-            }
-
-        }
-
-
-
-
-
-    }
-
-
 }
